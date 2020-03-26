c1a530fb72e6b18000fffb73b1029e7336463a2d
ONI David A <onidavid97@gmail.com>
2020-03-26 15:25:49 +0100

    removed developer exception page in production environment

--- ExpenseTracker.Web/Startup.cs
+++ ExpenseTracker.Web/Startup.cs
@@ -216,10 +216,10 @@ namespace ExpenseTracker.Web
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseExceptionHandler("/Home/Error");
+                //app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             //loggerFactory.AddFile("Logs/cyberspace{Date}.txt");
